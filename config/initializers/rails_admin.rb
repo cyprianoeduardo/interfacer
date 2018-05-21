383aea2938b72af2a87750d8dc3e9bc2431be9c9
cyprianoeduardo <cypriano.eduardo@gmail.com>
2018-05-21 04:16:36 +0000

    Added gem Rails_Admin

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,44 @@
+RailsAdmin.config do |config|
+
+  ### Popular gems integration
+
+  ## == Devise ==
+  # config.authenticate_with do
+  #   warden.authenticate! scope: :user
+  # end
+  # config.current_user_method(&:current_user)
+
+  ## == Cancan ==
+  # config.authorize_with :cancan
+
+  ## == Pundit ==
+  # config.authorize_with :pundit
+
+  ## == PaperTrail ==
+  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+
+  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+
+  #necessary to work with Rails 5.2. Reference: https://github.com/sferik/rails_admin/issues/3014
+  config.excluded_models = ['ActiveStorage::Blob', 'ActiveStorage::Attachment']
+
+  ## == Gravatar integration ==
+  ## To disable Gravatar integration in Navigation Bar set to false
+  # config.show_gravatar = true
+
+  config.actions do
+    dashboard                     # mandatory
+    index                         # mandatory
+    new
+    export
+    bulk_delete
+    show
+    edit
+    delete
+    show_in_app
+
+    ## With an audit adapter, you can add:
+    # history_index
+    # history_show
+  end
+end
