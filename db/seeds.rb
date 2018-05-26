07458db686e3516024165a4cf4c26455a46299a0
cyprianoeduardo <cypriano.eduardo@gmail.com>
2018-05-26 12:57:34 -0300

    Added default Company and User(admin) and some examples for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# default company, administrator
+petrobras = Company.create name: 'Petrobras', contract_number: 123456789
+# default administrator
+User.create first_name: 'sysadmin', last_name: 'Petrobras', email: 'sysadmin@petrobras.com', password: 123456, kind: :admin, status: :active, company: petrobras
+
+
+# for tests
+cooec = Company.create name: 'Cooec', contract_number: 987654321
+jurong = Company.create name: 'Jurong', contract_number: 1029384756
+altus = Company.create name: 'Altus', contract_number: 9627351648
+User.create first_name: 'Eduardo', last_name: 'Cypriano', email: 'eduardo@cooec.com', password: 123456, kind: :default, status: :active, company: cooec
+User.create first_name: 'Munique', last_name: 'Araújo', email: 'munique@altus.com', password: 123456, kind: :default, status: :active, company: altus
+User.create first_name: 'Guilherme', last_name: 'Cypriano', email: 'guilherme@altus.com', password: 123456, kind: :default, status: :active, company: altus
+User.create first_name: 'Judith', last_name: 'Cypriano', email: 'judith@jurong.com', password: 123456, kind: :default, status: :active, company: jurong
+User.create first_name: 'Victor Hugo', last_name: 'Cypriano', email: 'vh@jurong.com', password: 123456, kind: :default, status: :inactive, company: jurong
