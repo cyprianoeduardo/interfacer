f395aef5f88e46fed8d4a6f9cd52902d5cc89bb1
cyprianoeduardo <cypriano.eduardo@gmail.com>
2018-05-27 18:47:29 -0300

    Fixed model  Company (tips by HugoTamaki)

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,7 @@
 class Company < ApplicationRecord
   has_many :users
   has_many :responses
-  has_many :requests
+  #has_many :requests
+  has_many :requesters_requests, foreign_key: :requester_id, class_name: 'Request'
+  has_many :requesteds_requests, foreign_key: :requested_id, class_name: 'Request'
 end
